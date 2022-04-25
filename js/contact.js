a310a689569fd0b1bf61c57f54906f9fe3186372
Fer Soria <mfersoria.59@gmail.com>
2022-04-25 13:47:33 -0300

    Contact

---
+++ js/contact.js
@@ -0,0 +1,35 @@
+const sendContact = document.getElementById('sendContact')
+const cancelContact = document.getElementById('cancelContact')
+const contactForm = document.getElementById('contactForm')
+const messageSent = document.getElementById('messageSent')
+let nameContact = document.getElementById('name-contact')
+let surnameContact = document.getElementById('surname-contact')
+
+
+
+sendContact.addEventListener('click', (event) => {
+    event.preventDefault()
+    const contactUser = {
+        nameContact: contactForm.elements.name.value,
+        surnameContact: contactForm.elements.surname.value,
+        emailContact: contactForm.elements.email.value,
+        messageContact: contactForm.elements.message.value,
+    }
+    localStorage.setItem('currentContactUser', JSON.stringify(contactUser))  
+    showMessageSent()
+    contactForm.reset()
+})
+
+
+function showMessageSent(message, element, time) {
+    messageSent.innerText = 'Su mensaje se envio correctamente';
+    setTimeout(() => {
+        messageSent.innerText = null
+    }, 2000)   
+}
+
+
+cancelContact.addEventListener('click',(event) => {
+    event.preventDefault()
+    contactForm.reset()
+})
