d2c4f041ff97cfa53f524f0c23fd6c9dd914c657
Walter Daniel <walterdcarrizo.19@gmail.com>
2022-05-10 09:55:17 -0300

    lista de canciones

---
+++ js/songList.js
@@ -0,0 +1,123 @@
+let songsList = [
+    {
+        artist: 'Don Osvaldo',
+        song: 'Misterios',
+        Album: 'Casi Justicia Social I',
+        gender: 'rock'
+    },
+    {
+        artist: 'Intoxicados',
+        song: 'Esta Saliendo El Sol',
+        Album: 'No Es Solo Rock Adn Roll',
+        gender: 'rock'
+    },
+    {
+        artist: 'Las Pastillas del Abuelo',
+        song: 'Otra Vuelta de Tuerca',
+        Album: 'Las Pastillas del Abuelo',
+        gender: 'rock'
+    },
+    {
+        artist: 'Ciro y los Persas',
+        song: 'Mirenla',
+        Album: '27',
+        gender: 'rock'
+    },
+    {
+        artist: 'Ala Delta',
+        song: 'Divididos',
+        Album: 'Acariciando lo Aspero',
+        gender: 'rock'
+    },
+    {
+        artist: 'Harry Styles',
+        song: 'As It Was',
+        Album: 'As It Was',
+        gender: 'Pop'
+    },
+    {
+        artist: 'TINI',
+        song: 'Miénteme',
+        Album: 'Miénteme',
+        gender: 'Pop'
+    },
+    {
+        artist: 'Elton John',
+        song: 'Cold Heart',
+        Album: 'Cold Heart',
+        gender: 'Pop'
+    },
+    {
+        artist: 'Maluma',
+        song: 'Hawái',
+        Album: 'Hawái',
+        gender: 'Pop'
+    },
+    {
+        artist: 'Madonna',
+        song: 'Material Girl',
+        Album: 'Madonna',
+        gender: 'Pop'
+    },
+    {
+        artist: 'Chaqueño Palavecino',
+        song: 'La Ley y La Trampa',
+        Album: 'La Ley y La Trampa',
+        gender: 'Folclore'
+    },
+    {
+        artist: 'Soledad',
+        song: 'Propiedad Privada',
+        Album: 'Soledad',
+        gender: 'Folclore'
+    },
+    {
+        artist: 'Campedrinos',
+        song: 'Eterno Amor',
+        Album: 'Eterno Amor',
+        gender: 'Folclore'
+    },
+    {
+        artist: 'Horacio Guarany',
+        song: 'Del Chucaro',
+        Album: 'Horacio Guarany',
+        gender: 'Folclore'
+    },
+    {
+        artist: 'Los Huayra',
+        song: 'La noche Sin Tí',
+        Album: 'La Voz del Viento',
+        gender: 'Folclore'
+    },
+    {
+        artist: 'Marama',
+        song: 'Ya no Llora',
+        Album: 'Ya no Llora',
+        gender: 'Cumbia'
+    },
+    {
+        artist: 'La Fase Buk',
+        song: 'Marte',
+        Album: 'Marte',
+        gender: 'Cumbia'
+    },
+    {
+        artist: 'Matías Valdez',
+        song: 'Boleto al Amor',
+        Album: 'Boleto al Amor',
+        gender: 'Cumbia'
+    },
+    {
+        artist: 'Ke Kumbia',
+        song: 'Desde Mis Ojos',
+        Album: 'Desde Mis Ojos',
+        gender: 'Cumbia'
+    },
+    {
+        artist: 'Migrantes',
+        song: 'Hoy Volvi a Verte',
+        Album: 'Hoy Volvi a Verte',
+        gender: 'Cumbia'
+    }
+];
+    
