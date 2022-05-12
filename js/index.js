18bc6e5d7a5e2498783342e98e569f448c909fc5
lucasruizok <lucaruiz734@gmail.com>
2022-05-11 21:02:55 -0300

    aumento de carousel

--- js/index.js
+++ js/index.js
@@ -1,17 +1,15 @@
-const navbar = document.querySelector('.navbar');
-window.addEventListener('scroll', function(){
-      navbar.classList.toggle('active', this.window.scrollY > 0);
-});
+let items = document.querySelectorAll('.carousel .carousel-item')
 
-
-window.addEventListener('load', function(){
-     new Glinder(document.querySelector('.carousel__lista'), {
-        slidesToShow: 5,
-        slidesToScroll: 5,
-        dots: '.carouselIndicadores',
-        arrows: {
-          prev: '.carouselAnterior',
-          next: '.carouselSiguiente'
-        }
-     });
-});
+items.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
