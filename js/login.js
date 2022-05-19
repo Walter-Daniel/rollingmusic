a30ce675096d39692ce11cced2d97059d5fc35ee
Walter Daniel <walterdcarrizo.19@gmail.com>
2022-05-19 17:25:09 -0300

    primeros cambios

--- js/login.js
+++ js/login.js
@@ -82,14 +82,14 @@ formLogin.addEventListener('submit', (event) => {
     
         if(user.role === 'CLIENT'){
             localStorage.setItem('currentUser', JSON.stringify(user))
+            formLogin.reset();
+            checkIsAuth();
+            window.location.href = '/index.html'
             Swal.fire(
                 'Good job!',
                 'You clicked the button!',
                 'success'
               )
-            formLogin.reset();
-            checkIsAuth();
-            window.location.href = '/index.html'
         }else if(user.role === 'ADMIN'){
             localStorage.setItem('administrator', JSON.stringify(user))
             formLogin.reset();
@@ -111,24 +111,3 @@ function showErrorMsgL(message, time = 3000){
         loginError.textContent = null;
     }, time)
 };
-
-        // Example starter JavaScript for disabling form submissions if there are invalid fields
-        (function () {
-            'use strict'
-          
-            // Fetch all the forms we want to apply custom Bootstrap validation styles to
-            var forms = document.querySelectorAll('.needs-validation')
-          
-            // Loop over them and prevent submission
-            Array.prototype.slice.call(forms)
-              .forEach(function (form) {
-                form.addEventListener('submit', function (event) {
-                  if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                  }
-          
-                  form.classList.add('was-validated')
-                }, false)
-              })
-          })()
