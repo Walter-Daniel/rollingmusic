8092f5d2a7cb33a48968879db0fe9356cd436d22
Walter Daniel <walterdcarrizo.19@gmail.com>
2022-05-19 12:32:38 -0300

    css y footer

--- js/adminMusic.js
+++ js/adminMusic.js
@@ -1,97 +1,20 @@
-// adminCheck();
-// function adminCheck(){
-//     admin = JSON.parse(localStorage.getItem('administrator'));
-//     if(admin){
-//         login.classList.add('dnone')
-//         logout.classList.remove('dnone')
-//         adminMusic.classList.remove('dnone')
-//         adminUsers.classList.remove('dnone')
-//     } 
-// };
-
-// logout.addEventListener('click', () => {
-//     localStorage.removeItem('administrator');
-//     adminCheck();
-//     window.location.href = '/pages/login.html'
-// })
-
-// const formSong = document.getElementById('formSong')
-// const element = formSong.elements
-// const code = document.getElementById('code')
-// const nameSong = document.getElementById('nameSong')
-// const artist = document.getElementById('artist')
-// const album = document.getElementById('album')
-// const gen = document.getElementById('gen')
-// const year = document.getElementById('year')
-// const modalIngresos = document.getElementById('modalIngresos')
-// const imgSong = document.getElementById('img-song')
-// const closeModal = document.getElementById('closeModal')
-// const songsList = document.getElementById('songsList')
-
-// modalIngresos.addEventListener('submit', (e) => {
-//     e.preventDefault()    
-//     song = {
-//         nameSong: element.nameSong.value,
-//         artist: element.artist.value,
-//         album: element.album.value,
-//         gen: element.gen.value,
-//         year: element.year.value,
-//         // imgSong: element.imgSong.value,
-//         code: new Date()
-//     }
-//     console.log(song)
-//     localStorage.setItem('song', JSON.stringify(song))
-//     formSong.reset()
-//     renderSong()
-
-// })
-
-// closeModal.addEventListener('click', (event) => {
-//     event.preventDefault()
-//     formSong.reset()
-// })
-
-// song = JSON.parse(song)
-// console.log(song)
-
-// function renderSong() {
-//     const songs = JSON.parse(localStorage.getItem('songs')) || [];
-//     songsList.innerHTML = ''
-//     songs.forEach(song => {
-//         songsList.innerHTML += `<li>
-//                 // <img class="avatar" src=${user.avatar.includes('http') ? user.avatar : '/assets/image/users/default-avatar.png'}></div>
-//                 <div class="data">
-//                     <div  class="nameSong"> ${songs.nameSong}</div>
-//                     <div  class="artist"> ${songs.artist}</div>   
-//                 </div>
-//                 <div class="data">
-//                     <div  class="album"> ${songs.album}</div>  
-//                     <div class="year">${songs.year}</div>
-//                 </div>
-//                 <div class="gen">${songs.gen}</div>
-//                 <div class="code">${songs.code}</div>
-//                 </li>`
-//     })
-// }
-
-// ==================================
-
 adminCheck();
-
-function adminCheck() {
+function adminCheck(){
     admin = JSON.parse(localStorage.getItem('administrator'));
-    if (admin) {
+    if(admin){
         login.classList.add('dnone')
         logout.classList.remove('dnone')
         adminMusic.classList.remove('dnone')
         adminUsers.classList.remove('dnone')
-    }
+    } 
 };
+
 logout.addEventListener('click', () => {
     localStorage.removeItem('administrator');
     adminCheck();
     window.location.href = '/pages/login.html'
 })
+
 const formSong = document.getElementById('formSong')
 const element = formSong.elements
 const code = document.getElementById('code')
@@ -105,6 +28,8 @@ const imgSong = document.getElementById('img-song')
 const closeModal = document.getElementById('closeModal')
 const songsList = document.getElementById('songsList')
 let songs = JSON.parse(localStorage.getItem('songs')) || [];
+
+
 modalIngresos.addEventListener('submit', (e) => {
     e.preventDefault()
     const song = {
@@ -113,7 +38,7 @@ modalIngresos.addEventListener('submit', (e) => {
         album: element.album.value,
         gen: element.gen.value,
         year: element.year.value,
-        // imgSong: element.imgSong.value,
+        imgSong: element.imgSong.value,
         code: new Date()
     }
     songs.push(song)
@@ -131,6 +56,7 @@ closeModal.addEventListener('click', (event) => {
     formSong.reset()
 })
 
+let number = 1;
 function renderSong() {
     const songs = JSON.parse(localStorage.getItem('songs')) || [];
     songsList.innerHTML = ''
@@ -143,13 +69,30 @@ function renderSong() {
 
             <td>${song.code}</td>
             <td>
-                <p>${song.nameSong}</p>
-                <p>${song.artist}</p>
+                ${song.nameSong}
+            </td>
+            <td>
+                ${song.artist}
             </td>
             <td>${song.album}</td>
             <td>${song.gen}</td>
             <td>${song.year}</td>
-        </tr>`
+            <td>
+            <button type="button" class="btn btn-ingresos">
+            <i class="fa-solid fa-star"></i>
+            </button>
+            
+            <button type="button" class="btn btn-ingresos" data-bs-toggle="modal" data-bs-target="#${song.nameSong}">
+            <i class="fa-solid fa-pen-to-square"></i>
+            </button>
+            <button type="button" class="btn btn-ingresos">
+            <i class="fa-solid fa-trash-can"></i>
+            </button>
+            </td>
+
+        </tr>
+        
+        `
     })
 }
 renderSong();
